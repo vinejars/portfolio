0089d268a80df3ce0d069a8a7d5f1355f66031a6
Em Comeau <comeau.amanda.n@gmail.com>
2021-07-13 16:05:27 -0400

    testimonials working, needs styling

--- client/components/Testimonials.js
+++ client/components/Testimonials.js
@@ -2,16 +2,24 @@ import React from 'react'
 
 export default function Testimonials() {
     return (
-        <div>
-            <div id='kelsey'>
-           <h4><i>Em is an amazing developer to work with. They came to our project full of ideas and enthusiasm and shaped the vision 
+            <div class="testcontainer">
+  <div class="kelsgif"><img src='/kelsey.gif' width={200}/></div>
+  <div class="kelstest"><h4><i>Em is an amazing developer to work with. They came to our project full of ideas and enthusiasm and shaped the vision 
                of the app from the start. Their creativity and engineering expertise were crucial throughout the development of our app, 
                from storyboarding to database creation to styling to AWS integration. They were always eager to jump into unknown 
                technologies and never shied away from trying something new. Em brought so much fun to the group and their contagious 
                personality made every day working together enjoyable. Their technical skills, originality, and inventiveness make them an 
-               exceptional developer. I would highly recommend Em for any software engineering project! </i> </h4> 
-           <h2>- Kelsey, fellow web developer at tagged, an AR mobile app</h2>
-           </div>
-        </div>
+               exceptional developer. I would highly recommend Em for any software engineering project! </i> </h4> <br/> <a href='https://linkedin.com/in/kelsey-siman'> Kelsey's LinkedIn</a></div>
+  <div class="karigif"><img src='/karina.gif' width={200}/></div>
+  <div class="karitest"><h4><i>Em was a pleasure to work with! They are very knowledgeable when it comes to AWS/S3, React, and Redux. They also have an amazing eye 
+  for design and go the extra mile to make everything look good. They are a great team player and always willing to help and/or provide insight.</i></h4> <br/> <a href='https://www.linkedin.com/in/karina-zuniga/'> Karina's LinkedIn</a></div>
+  <div class="katgif"><img src='/kat.gif' width={200}/></div>
+  <div class="kattest"><h4><i>I recently had the pleasure of working with Em during my time as a fellow for the Grace Hopper Program. 
+  Em is an inquisitive and diligent software engineer who can pick up new concepts rapidly. From the beginning, I was impressed with 
+  how Em consistently worked to deepen their knowledge. They demonstrated curiosity and initiative on multiple occasions, including 
+  learning Amazon S3 for a solo project. I highly recommend Em as a software engineer and believe they will be an asset to any team 
+  due to their persistence, clear communication, and humor.</i></h4> <br/> <a href='https://www.linkedin.com/in/yangkb09/'> Kat's LinkedIn</a></div>
+</div>
+           
     )
 }
--- public/bundle.js
+++ public/bundle.js
@@ -903,9 +903,36 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
 
 function Testimonials() {
-  return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", {
-    id: "kelsey"
-  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("h4", null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("i", null, "Em is an amazing developer to work with. They came to our project full of ideas and enthusiasm and shaped the vision of the app from the start. Their creativity and engineering expertise were crucial throughout the development of our app, from storyboarding to database creation to styling to AWS integration. They were always eager to jump into unknown technologies and never shied away from trying something new. Em brought so much fun to the group and their contagious personality made every day working together enjoyable. Their technical skills, originality, and inventiveness make them an exceptional developer. I would highly recommend Em for any software engineering project! "), " "), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("h2", null, "- Kelsey, fellow web developer at tagged, an AR mobile app")));
+  return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", {
+    class: "testcontainer"
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", {
+    class: "kelsgif"
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("img", {
+    src: "/kelsey.gif",
+    width: 200
+  })), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", {
+    class: "kelstest"
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("h4", null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("i", null, "Em is an amazing developer to work with. They came to our project full of ideas and enthusiasm and shaped the vision of the app from the start. Their creativity and engineering expertise were crucial throughout the development of our app, from storyboarding to database creation to styling to AWS integration. They were always eager to jump into unknown technologies and never shied away from trying something new. Em brought so much fun to the group and their contagious personality made every day working together enjoyable. Their technical skills, originality, and inventiveness make them an exceptional developer. I would highly recommend Em for any software engineering project! "), " "), " ", /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("br", null), " ", /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("a", {
+    href: "https://linkedin.com/in/kelsey-siman"
+  }, " Kelsey's LinkedIn")), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", {
+    class: "karigif"
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("img", {
+    src: "/karina.gif",
+    width: 200
+  })), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", {
+    class: "karitest"
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("h4", null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("i", null, "Em was a pleasure to work with! They are very knowledgeable when it comes to AWS/S3, React, and Redux. They also have an amazing eye for design and go the extra mile to make everything look good. They are a great team player and always willing to help and/or provide insight.")), " ", /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("br", null), " ", /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("a", {
+    href: "https://www.linkedin.com/in/karina-zuniga/"
+  }, " Karina's LinkedIn")), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", {
+    class: "katgif"
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("img", {
+    src: "/kat.gif",
+    width: 200
+  })), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", {
+    class: "kattest"
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("h4", null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("i", null, "I recently had the pleasure of working with Em during my time as a fellow for the Grace Hopper Program. Em is an inquisitive and diligent software engineer who can pick up new concepts rapidly. From the beginning, I was impressed with how Em consistently worked to deepen their knowledge. They demonstrated curiosity and initiative on multiple occasions, including learning Amazon S3 for a solo project. I highly recommend Em as a software engineer and believe they will be an asset to any team due to their persistence, clear communication, and humor.")), " ", /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("br", null), " ", /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("a", {
+    href: "https://www.linkedin.com/in/yangkb09/"
+  }, " Kat's LinkedIn")));
 }
 
 /***/ }),
