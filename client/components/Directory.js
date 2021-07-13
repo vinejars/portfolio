72e74f5f837c511bee61ba05a5dbcb3c7da40d74
Em Comeau <comeau.amanda.n@gmail.com>
2021-07-13 17:57:37 -0400

    fix: broken link

--- client/components/Directory.js
+++ client/components/Directory.js
@@ -15,7 +15,7 @@ export default function Directory() {
        <div className='aboutme'><Link to='/aboutme'> <motion.img whileHover={{ scale: 1.2}}  src='/aboutmelink.png' alt={'about me link'}/>  </Link> </div>
        <div className='tech'><Link to='/tech'> <motion.img whileHover={{ scale: 1.2}}  src='/techlink.png' alt={'tech link'}/>  </Link></div>
        <div className='social'><Link to='/social'> <motion.img whileHover={{ scale: 1.2}}  src='/sociallink.png' alt={'social media link'}/> </Link></div>
-       <div className='testimonials'> <Link to='/testimonals'> <motion.img whileHover={{ scale: 1.2}}  src='/testimonialslink.png' alt={'testimonials link'}/></Link></div>
+       <div className='testimonials'> <Link to='/testimonials'> <motion.img whileHover={{ scale: 1.2}}  src='/testimonialslink.png' alt={'testimonials link'}/></Link></div>
        <div className='home'><Link to='/main'><motion.img whileHover={{ scale: 1.2}}  src='homelink.png' alt={'home link'}/></Link></div>
         </div>
         </div>
